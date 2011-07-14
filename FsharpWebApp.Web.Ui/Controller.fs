95ac12660d5defd310fea5559a5382bdceeb2a48
Matthew Taylor <luv2code@gmail.com>
2011-07-13 21:52:56 -0500

    Stream lined some more of it to get ready for article.

---
+++ FsharpWebApp.Web.Ui/Controller.fs
@@ -0,0 +1,34 @@
+﻿module Routing
+    open System
+    open Nancy
+    open NancyExt
+    type RouteModule () as self = 
+        inherit NancyModule ()
+        do
+            self.Get.["/{name}"] <- ToActionFunc self.RootPath
+            self.Post.["/{name}"] <- ToActionFunc self.RootPath
+
+            self.Get.["/AsyncTest"] <- ToActionFunc self.AsyncTest
+
+        member self.RootPath (urlParams) = 
+            let name = (urlParams:?>DynamicDictionary)?name.ToString();
+            let model = new DynamicDictionary()
+            let message = 
+                match self.Request.Method with
+                | "GET" -> "Hello World"
+                | "POST" -> "this was posted back: " + ((self.Request.Form:?>DynamicDictionary)?userMessage:?>string)
+                | _ -> "Unsupported Method"
+            model?Message <- message
+            model?UrlMessage <- if String.IsNullOrWhiteSpace(name) then "" else "this was on the url : " + name 
+            model?Time <- DateTime.Now
+            self.RenderView("index.cshtml", model)
+
+        member self.AsyncTest urlParams = 
+            let res = 
+                async {
+                    do! Async.Sleep 5000
+                    let! resp = async.Return(Response.FromString("hello"))
+                    return resp
+                } |> Async.StartAsTask
+            res.Wait()
+            res.Result
--- FsharpWebApp.Web.Ui/NancyExt.fs
+++ FsharpWebApp.Web.Ui/NancyExt.fs
@@ -1,19 +1,20 @@
 ﻿module NancyExt
     open Nancy
     type Nancy.Response with
-        static member FromString(text:string) = Response.op_Implicit(text)
-        static member FromView(view:System.Action<System.IO.Stream>) = Response.op_Implicit(view)
-        static member FromStatusCode(code:int) = Response.op_Implicit(code)
-        static member FromHttpStatusCode(code:HttpStatusCode) = Response.op_Implicit(code)
+        static member String(text:string) = Response.op_Implicit(text)
+        static member View(view:System.Action<System.IO.Stream>) = Response.op_Implicit(view)
+        static member StatusCode(code:int) = Response.op_Implicit(code)
+        static member HttpStatusCode(code:HttpStatusCode) = Response.op_Implicit(code)
     
     type Nancy.NancyModule with
-        member this.RenderView(viewName:string, model) = Response.FromView(this.View.[viewName, model])
+        member self.RenderView(viewName:string, model) = Response.View(self.View.[viewName, model])
   
-    let (?) (inst:obj) name = 
-        let dict = inst :?> Nancy.DynamicDictionary
-        let dictValue = dict.[name] :?> Nancy.DynamicDictionaryValue
+    let (?) (inst:Nancy.DynamicDictionary) name = 
+        let dictValue = inst.[name] :?> Nancy.DynamicDictionaryValue
         if dictValue.HasValue then dictValue.Value else null
 
-    let (?<-) (inst:obj) name value = 
-        let dict = inst :?> Nancy.DynamicDictionary
-        dict.[name] <- value
+    let (?<-) (inst:Nancy.DynamicDictionary) name value = 
+        inst.[name] <- value
+
+        
+    let ToActionFunc f = System.Func<System.Object,Nancy.Response> f
